80b5a2f15f5a4b9d767e6c05714e4ff1abbe4938
Nadide Ozturk <nadidebaca@gmail.com>
2018-01-13 22:49:41 -0800

    Initial Commit

---
+++ WhatToEatLocal/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  WhatToEatLocal
+//
+//  Created by Engin Oruc Ozturk on 13.01.2018.
+//  Copyright © 2018 NadideOzturk. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

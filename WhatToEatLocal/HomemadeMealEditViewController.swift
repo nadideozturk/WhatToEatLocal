2813b8d036a9ae7eb6be40c24b6e15162ca1d083
nadideozturk <nadidebaca@gmail.com>
2018-08-24 00:49:23 -0700

    empty edit views added with navigation

---
+++ WhatToEatLocal/HomemadeMealEditViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HomemadeMealEditViewController.swift
+//  WhatToEatLocal
+//
+//  Created by Engin Oruc Ozturk on 23.08.2018.
+//  Copyright © 2018 NadideOzturk. All rights reserved.
+//
+
+import UIKit
+
+class HomemadeMealEditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
